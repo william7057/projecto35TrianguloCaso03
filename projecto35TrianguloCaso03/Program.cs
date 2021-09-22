0167c1483c3a4652ad1be006e93cb6dad40a008c
william <factzure@hotmail.com>
2021-09-22 11:54:35 -0500

    Agregar archivos de proyecto.

---
+++ projecto35TrianguloCaso03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecto35TrianguloCaso03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limite = 1;
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int x = 1; x <= limite; x++)
+                {
+
+                    Console.Write("*");
+                }
+                limite = limite + 1;
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
